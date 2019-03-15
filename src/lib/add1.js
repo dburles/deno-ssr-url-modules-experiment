41f902acbed6ec61b6ceb7844084a6b235c97bd9
David Burles <dburles@gmail.com>
2019-03-15 20:22:14 +1100

    Delete add1.js

--- src/lib/add1.js
+++
@@ -1,7 +0,0 @@
-import get_random_int from './get_random_int.js';
-
-function add1 (number) {
-  return new Promise(resolve => setTimeout(function () { resolve(number + 1) }, get_random_int(0, 1000)));
-};
-
-export default add1;
