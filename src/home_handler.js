e31533e45d5bb111196a4a0fc94a986e4d501a9d
David Burles <dburles@gmail.com>
2019-03-15 16:37:50 +1100

    update

--- src/home_handler.js
+++ src/home_handler.js
@@ -5,17 +5,14 @@ import home_data from './home_data.js';
 export default create_handler(home, function ({ render }) {
   let count = 0;
 
-  async function counter_click_handler () {
-    count += 1;
-    render({ count });
-  };
-  async function refetch_click_handler () {
-    render({ data: await home_data() });
-  }
-
   return function mount () {
-    document.getElementById('counter').addEventListener('click', counter_click_handler);
-    document.getElementById('refetch').addEventListener('click', refetch_click_handler);
+    document.getElementById('counter').addEventListener('click', function () {
+      count += 1;
+      render({ count });
+    });
+    document.getElementById('refetch').addEventListener('click', async function () {
+      render({ data: await home_data() });
+    });
     
     return function unmount () {}
   }
--- src/page_one_handler.js
+++ src/page_one_handler.js
@@ -4,13 +4,11 @@ import page_one from './page_one.js';
 export default create_handler(page_one, function ({ render, navigate }) {
   let count = 0;
 
-  async function counter_click_handler () {
-    count += 1;
-    render({ count });
-  };
-
   return function mount () {
-    document.getElementById('counter').addEventListener('click', counter_click_handler);
+    document.getElementById('counter').addEventListener('click', function () {
+      count += 1;
+      render({ count });
+    });
     
     return function unmount () {}
   }
