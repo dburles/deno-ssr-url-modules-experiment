5135dfa47e909a0f8aee980805b846c2bed6b755
David Burles <dburles@gmail.com>
2019-03-15 22:58:55 +1100

    can't remember why this was that way

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 // TODO: dynamic import and use default export
 const routes = [
-  /^$/, () => ({ module_name: '../home' }),
-  /^page_one$/, () => ({ module_name: '../page_one' })
+  /^$/, () => ({ module_name: 'home' }),
+  /^page_one$/, () => ({ module_name: 'page_one' })
 ];
 
 export default routes;
